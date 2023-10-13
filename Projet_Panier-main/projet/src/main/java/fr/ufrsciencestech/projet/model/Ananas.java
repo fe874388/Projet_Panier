848897f81010298322433b8135d8abcf5c007f89
fe874388 <144998720+fe874388@users.noreply.github.com>
2023-10-13 12:49:27 +0200

    Add files via upload

---
+++ Projet_Panier-main/projet/src/main/java/fr/ufrsciencestech/projet/model/Ananas.java
@@ -0,0 +1,70 @@
+package fr.ufrsciencestech.projet.model;
+
+
+public class Ananas implements Fruit{
+    private double prix;
+    private String origine;
+	
+    public Ananas() 
+    {
+        this.prix = 0.5;  //prix en euros
+        this.origine="Espagne";
+    }
+    
+    public Ananas(double prix, String origine) 
+    {
+	if(prix < 0)
+	    this.prix = -prix;  //une solution possible pour interdire les prix negatifs
+	else
+	    this.prix = prix;
+
+	if(origine.equals(""))
+            this.origine = "Espagne";  //Espagne par défaut
+	else
+            this.origine = origine;   
+    }
+
+    public double getPrix(){
+	return prix;
+    }
+
+    public void setPrix(double prix){
+	this.prix=prix;
+    }
+
+    public String getOrigine(){
+	return origine;
+    }
+ 
+    public void setOrigine(String origine){
+	this.origine=origine;
+    }
+
+    @Override
+    public String toString(){
+        return "Ananas de " + origine + " a " + prix + " euros";
+    }
+
+    @Override
+    public boolean equals(Object o){  //predicat pour tester si 2 oranges sont equivalentes
+        if(o != null && getClass() == o.getClass()){
+            Ananas or = (Ananas) o;
+            return (prix == or.prix && origine.equals(or.origine));
+        }
+        return false;
+    }
+
+    public boolean isSeedless() {  //predicat indiquant qu'une orange a des pepins
+        return false;
+    }
+
+/*
+    public static void main (String[] args){
+        //Ecrire ici vos tests
+	System.out.println("premier test Ananas");
+    Ananas or = new Ananas(15,"algerie");
+    System.out.println(or);
+   }
+*/
+}
+
---
+++ Projet_Panier-main/projet/src/main/java/fr/ufrsciencestech/projet/view/VueConsole.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ufrsciencestech.projet.view;
+
+import fr.ufrsciencestech.projet.model.Modele;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.*;
+
+/**
+ *
+ * @author celine
+ */
+public class VueConsole implements PropertyChangeListener { //implements Observer{
+    private String trace;
+    
+    /**
+     * @return the trace
+     */
+    public String getTrace() {
+        return trace;
+    }
+
+    /**
+     * @param trace the trace to set
+     */
+    public void setTrace(String trace) {
+        this.trace = trace;
+    }
+    
+    public VueConsole(){
+        trace = "Valeur initiale : " + 0;
+        System.out.println(trace);
+    }
+    
+    //public void update(Observable m, Object compte){   //This method is called whenever the observed object is changed
+    //}
+    @Override
+    public void propertyChange(PropertyChangeEvent evt){
+        Modele m = (Modele) evt.getSource();
+        
+        trace = "Nouvelle valeur : " + m.getCompteur();
+        System.out.println(trace);
+        
+        /*System.out.println("Variation of " + evt.getPropertyName());
+	System.out.println("\t(" + evt.getOldValue() + 
+							" -> " + evt.getNewValue() + ")");
+	System.out.println("Property in object " + evt.getSource());*/
+    }
+}
